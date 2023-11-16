e551b8f94756be9082337a553d8161977a0b9446
hildisvini <hildisvini@gmail.com>
2023-11-16 10:56:27 +0100

    +kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 streamlit.header("Fruityvice Fruit Advice!")
 
